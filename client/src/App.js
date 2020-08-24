6cd11bb40853c0a92b43a72e8395aa46385b73b7
N Young <60322086+nai-young@users.noreply.github.com>
2020-08-24 20:52:58 +0100

    backend connection

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ListDay from './components/ListDay'
+
+function App() {
+  return (
+    <div className="App">
+        <ListDay/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/ListDay.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+ 
+export default class ListDay extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      /* date: new Date,
+      exercise: false,
+      coffe: 0,
+      water: 0,
+      study: 0,
+      work: 0 */
+      day: []
+    }
+  }
+  componentDidMount() {
+    axios.get('/')
+      .then(res => {
+        this.setState({
+          day: res.data
+        })
+      })
+      .catch(err => console.log(err))
+  }
+  render () {
+    return <div>
+      {this.state.day}
+    </div>
+  }
+}
